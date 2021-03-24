316b33aeb6a68acf049819dad5d461105e66e9ee
mhd53 <mhd53@users.noreply.github.com>
2021-03-24 10:37:50 -0400

    test(auth): add all tests for auth service!

--- internal/auth/auth_service_test.go
+++ internal/auth/auth_service_test.go
@@ -11,17 +11,7 @@ func TestRegisterWhenUserExists(t *testing.T) {
 	mockStore := new(MockStore)
 
 	var id int64 = 1
-	user := entity.User{
-		ID: id,
-		BaseUser: entity.BaseUser{
-			Username: MOCK_USERNAME,
-			Email:    MOCK_EMAIL,
-			Password: MOCK_PWD,
-		},
-		Weight: MOCK_WEIGHT,
-		Height: MOCK_HEIGHT,
-		Gender: MOCK_GENDER,
-	}
+	user := CreateValidMockUser(id)
 
 	mockStore.On("FindUserByUsername").Return(user, true, nil)
 	mockStore.On("Save").Return(user, nil)
@@ -32,6 +22,7 @@ func TestRegisterWhenUserExists(t *testing.T) {
 	token, err := testService.Register(MOCK_USERNAME, MOCK_EMAIL, MOCK_PWD, MOCK_PWD)
 
 	assert.NotNil(t, err)
+	assert.Equal(t, "User already exists!", err.Error())
 	assert.Empty(t, token)
 }
 
@@ -39,17 +30,7 @@ func TestRegisterSuccess(t *testing.T) {
 	mockStore := new(MockStore)
 
 	var id int64 = 1
-	user := entity.User{
-		ID: id,
-		BaseUser: entity.BaseUser{
-			Username: MOCK_USERNAME,
-			Email:    MOCK_EMAIL,
-			Password: MOCK_PWD,
-		},
-		Weight: MOCK_WEIGHT,
-		Height: MOCK_HEIGHT,
-		Gender: MOCK_GENDER,
-	}
+	user := CreateValidMockUser(id)
 
 	mockStore.On("FindUserByUsername").Return(entity.User{}, false, nil)
 	mockStore.On("Save").Return(user, nil)
@@ -63,29 +44,126 @@ func TestRegisterSuccess(t *testing.T) {
 	assert.NotEmpty(t, token)
 }
 
-func TestLoginWithUname(t *testing.T) {
+func TestLoginWithUnameWhenUserNotExist(t *testing.T) {
+	mockStore := new(MockStore)
+
+	mockStore.On("FindUserByUsername").Return(entity.User{}, false, nil)
+	mockStore.On("Save").Return(entity.User{}, nil)
+
+	testValidator := NewAuthValidator(mockStore)
+	testService := NewAuthService(mockStore, testValidator)
+
+	token, err := testService.LoginWithUname(MOCK_USERNAME, MOCK_PWD)
+
+	assert.NotNil(t, err)
+	assert.Equal(t, "Username doesn't exist!", err.Error())
+	assert.Empty(t, token)
+}
+
+func TestLoginWithUnameWhenIncorrectPwd(t *testing.T) {
 	mockStore := new(MockStore)
 
 	var id int64 = 1
-	user := entity.User{
-		ID: id,
-		BaseUser: entity.BaseUser{
-			Username: MOCK_USERNAME,
-			Email:    MOCK_EMAIL,
-			Password: MOCK_PWD,
-		},
-		Weight: MOCK_WEIGHT,
-		Height: MOCK_HEIGHT,
-		Gender: MOCK_GENDER,
-	}
+	user := CreateValidMockUser(id)
 
-	mockStore.On("FindUserByUsername").Return(entity.User{}, false, nil)
+	mockStore.On("FindUserByUsername").Return(user, true, nil)
 	mockStore.On("Save").Return(user, nil)
 
 	testValidator := NewAuthValidator(mockStore)
 	testService := NewAuthService(mockStore, testValidator)
 
-	token, err := testService.Register(MOCK_USERNAME, MOCK_EMAIL, MOCK_PWD, MOCK_PWD)
+	token, err := testService.LoginWithUname(MOCK_USERNAME, "bobin")
+
+	assert.NotNil(t, err)
+	assert.Equal(t, "Incorrect password!", err.Error())
+	assert.Empty(t, token)
+}
+
+func TestLoginWithUnameSuccess(t *testing.T) {
+	mockStore := new(MockStore)
+
+	var id int64 = 1
+	user := CreateValidMockUser(id)
+
+	mockStore.On("FindUserByUsername").Return(user, true, nil)
+	mockStore.On("Save").Return(user, nil)
+
+	testValidator := NewAuthValidator(mockStore)
+	testService := NewAuthService(mockStore, testValidator)
+
+	token, err := testService.LoginWithUname(MOCK_USERNAME, MOCK_PWD)
+
+	assert.Nil(t, err)
+	assert.NotEmpty(t, token)
+}
+
+func TestLoginWithEmailWhenUserNotExist(t *testing.T) {
+	mockStore := new(MockStore)
+
+	mockStore.On("FindUserByEmail").Return(entity.User{}, false, nil)
+	mockStore.On("Save").Return(entity.User{}, nil)
+
+	testValidator := NewAuthValidator(mockStore)
+	testService := NewAuthService(mockStore, testValidator)
+
+	token, err := testService.LoginWithEmail(MOCK_EMAIL, MOCK_PWD)
+
+	assert.NotNil(t, err)
+	assert.Equal(t, "Email doesn't exist!", err.Error())
+	assert.Empty(t, token)
+}
+
+func TestLoginWithEmailWhenIncorrectPwd(t *testing.T) {
+	mockStore := new(MockStore)
+
+	var id int64 = 1
+	user := CreateValidMockUser(id)
+
+	mockStore.On("FindUserByEmail").Return(user, true, nil)
+	mockStore.On("Save").Return(user, nil)
+
+	testValidator := NewAuthValidator(mockStore)
+	testService := NewAuthService(mockStore, testValidator)
+
+	token, err := testService.LoginWithEmail(MOCK_EMAIL, "bobin")
+
+	assert.NotNil(t, err)
+	assert.Equal(t, "Incorrect password!", err.Error())
+	assert.Empty(t, token)
+}
+
+func TestLoginWithEmailWhenInvalidEmail(t *testing.T) {
+	mockStore := new(MockStore)
+
+	var id int64 = 1
+	user := CreateValidMockUser(id)
+
+	mockStore.On("FindUserByEmail").Return(user, true, nil)
+	mockStore.On("Save").Return(user, nil)
+
+	testValidator := NewAuthValidator(mockStore)
+	testService := NewAuthService(mockStore, testValidator)
+
+	token, err := testService.LoginWithEmail("notanemail", "bobin")
+
+	assert.NotNil(t, err)
+	assert.Equal(t, "Invalid email!", err.Error())
+	assert.Empty(t, token)
+}
+
+func TestLoginWithEmailSuccess(t *testing.T) {
+	mockStore := new(MockStore)
+
+	var id int64 = 1
+	user := CreateValidMockUser(id)
+
+	mockStore.On("FindUserByEmail").Return(user, true, nil)
+	mockStore.On("Save").Return(user, nil)
+
+	testValidator := NewAuthValidator(mockStore)
+	testService := NewAuthService(mockStore, testValidator)
+
+	token, err := testService.LoginWithEmail(MOCK_EMAIL, MOCK_PWD)
 
 	assert.Nil(t, err)
 	assert.NotEmpty(t, token)
