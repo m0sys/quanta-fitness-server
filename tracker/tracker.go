6177ea2b44f529b3530c50c1468b9525923255ff
mhd53 <mhd53@users.noreply.github.com>
2021-04-24 06:44:55 -0400

    feat(uc): add ath add exercise

--- tracker/tracker.go
+++ tracker/tracker.go
@@ -16,12 +16,12 @@ type WorkoutTracker interface {
 	AddExerciseToWorkoutLog(req AddExerciseToWorkoutLogReq) (ExerciseRes, error)
 }
 
+// FIXME: Get rid of that pointer for ath.
 type tracker struct {
 	repo Repository
 	ath  *athlete.Athlete
+	wlog *wl.WorkoutLog
 }
-
-// FIXME: Get rid of that pointer for ath.
 
 // NewTracker create a new WorkoutTracker for Athlete.
 func NewTracker(repository Repository, athlete *athlete.Athlete) WorkoutTracker {
@@ -47,56 +47,29 @@ func (t *tracker) CreateWorkoutLog(title string) (WorkoutLogRes, error) {
 		log.Printf("%s: couldn't store WorkoutLog: %s", "tracker", err.Error())
 	}
 
+	t.wlog = &wlog
 	return repoWlog, nil
 }
 
 func (t *tracker) AddExerciseToWorkoutLog(req AddExerciseToWorkoutLogReq) (ExerciseRes, error) {
-	wlogRes, found, err := t.repo.FindWorkoutLogByID(req.LogID)
-	if err != nil {
-		log.Printf("error while searching for WorkoutLog in repo: %s", err.Error())
-		return ExerciseRes{}, errors.New("Internal Error")
+	if t.wlog == nil {
+		return ExerciseRes{}, errors.New("no WorkoutLog is assigned to Tracker")
+	}
+	if t.wlog.LogID != req.LogID {
+		return ExerciseRes{}, errors.New("WorkoutLog does not match requested LogID")
 	}
 
-	if !found {
-		return ExerciseRes{}, errors.New("Workout Log not found")
-	}
-
-	wlog := wl.WorkoutLog{
-		LogID: wlogRes.LogID,
-		Title: wlogRes.Title,
-		Date:  wlogRes.Date,
-	}
-
-	exercisesRes, err := t.repo.FindAllExercisesForWorkoutLog(wlog)
-	if err != nil {
-		log.Printf("error while fetching all Exercises for WorkoutLog in repo: %s", err.Error())
-		return ExerciseRes{}, errors.New("Internal Error")
-	}
-
-	var exercises []wl.Exercise
-	for _, e := range exercisesRes {
-		exercise := wl.Exercise{
-			ExerciseID: e.ExerciseID,
-			Name:       e.Name,
-			Weight:     e.Weight,
-			TargetRep:  e.TargetRep,
-			RestTime:   e.RestTime,
-		}
-		exercises = append(exercises, exercise)
-	}
-	wlog.Exercises = exercises
-
-	newExercise, err := wl.NewExercise(req.Name, req.Weight, req.RestTime, req.TargetRep, len(exercises))
+	newExercise, err := wl.NewExercise(req.Name, req.Weight, req.RestTime, req.TargetRep, len(t.wlog.Exercises))
 	if err != nil {
 		return ExerciseRes{}, err
 	}
 
-	err = wlog.AddExercise(newExercise)
+	err = t.wlog.AddExercise(newExercise)
 	if err != nil {
 		return ExerciseRes{}, err
 	}
 
-	exerciseRes, err := t.repo.AddExerciseToWorkoutLog(wlog, newExercise)
+	exerciseRes, err := t.repo.AddExerciseToWorkoutLog(*t.wlog, newExercise)
 	if err != nil {
 		log.Printf("error while adding Exercise to WorkoutLog in repo: %s", err.Error())
 		return ExerciseRes{}, errors.New("Internal Error")
